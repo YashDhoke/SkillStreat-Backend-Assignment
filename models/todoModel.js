8940dc379740cd95b2e81bea088aceba63722d7a
YashDhoke <yashdhoke1@gmail.com>
2024-01-03 00:48:00 +0530

    added data validation to ensure that note title and description are of a certain minimum length

--- models/todoModel.js
+++ models/todoModel.js
@@ -4,11 +4,13 @@ const todoSchema = mongoose.Schema(
     {
         title: {
             type: String,
-            required: [true, "Please enter Title"]
+            required: [true, "Please enter Title"] ,
+            minlength: [3, "Title should be at least 3 characters long"]
         },
         description: {
             type: String,
-            required: [true,"Please enter the description"]
+            required: [true,"Please enter the description"] , 
+            minlength: [10, "Description should be at least 3 characters long"]
         }
     },
     {
