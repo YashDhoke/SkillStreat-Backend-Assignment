1721a36f0ad8a20cef64d7b201dd4eabc6bc78e3
YashDhoke <yashdhoke1@gmail.com>
2024-01-03 00:21:08 +0530

    added the get notes feature and validated with postman

--- server.js
+++ server.js
@@ -13,7 +13,16 @@ app.get('/' ,  (req , res) => {
        res.send('Hello') ; 
 }); 
 
-app.post('/todos' , async(req , res) => {
+app.get('/todos' , async(req , res) => {
+    try{
+         const todos = await Todo.find({}) ; 
+         res.status(200).json(todos) ;  
+    }catch(error){
+        res.status(500).json({message : error.message}) ; 
+    }
+})
+
+app.post('/todo' , async(req , res) => {
     try{
         const todo = await Todo.create(req.body) ; 
         res.status(200).json(todo) ; 
